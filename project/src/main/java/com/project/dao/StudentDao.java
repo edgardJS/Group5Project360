6ee63e0595f3fa4499755883d345c1af8c075b7b
Brian Jorgenson <brianjor@uw.edu>
2016-11-30 13:42:19 -0800

    Working testAddStudent

--- project/src/main/java/com/project/dao/StudentDao.java
+++ project/src/main/java/com/project/dao/StudentDao.java
@@ -1,10 +1,14 @@
 package com.project.dao;
 
+import com.project.Model.Degree;
+import com.project.Model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.sql.PreparedStatement;
+import java.sql.Types;
 
 
 /**
@@ -21,11 +25,26 @@ public class StudentDao {
         System.out.println(rowCount);
     }
 
+    public void testAddDegree() {
+        String sql = "insert into `Degree`(`degree`, graduationTerm," +
+                " graduationYear, program) values "
+                + "(?, ?, ?, ?)";
+        Object[] parameters = {"degree", "term", 2010, "program"};
+        int[] types = {Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.INTEGER};
+        jdbcTemplate.update(sql, parameters, types);
+    }
+
     public void testAddStudent() {
-        String sql = "insert into Degree(studentId, degree, graduationTerm," +
-                " graduationYear) values "
-                + "(?, ?, ?, ?); ";
-        PreparedStatement stmt = null;
+        String sql = "insert into Student(studentId, lastName, firstName, uwEmail, externalEmail, " +
+                "transferColleges, gpa) values "
+                + "(?, ?, ?, ?, ?, ?, ?)";
+        Object[] parameters = {12345, "lstname", "frstname", "@email", "@external", "transfers", 4};
+        int[] types = {Types.INTEGER, Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.VARCHAR,
+                Types.VARCHAR, Types.INTEGER};
+        jdbcTemplate.update(sql, parameters, types);
+    }
+
+    public void testGetStudent() {
 
     }
 }
