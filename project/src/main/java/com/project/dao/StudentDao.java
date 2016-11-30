b57cf93b3762ce40a761a2d15f3b4073c1a11e43
Brian Jorgenson <brianjor@uw.edu>
2016-11-30 10:53:40 -0800

    Testing adding to database

--- project/src/main/java/com/project/dao/StudentDao.java
+++ project/src/main/java/com/project/dao/StudentDao.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+
 
 /**
  * Created by edgards on 11/27/16.
@@ -17,5 +19,13 @@ public class StudentDao {
     public void testGetItems() {
         int rowCount = this.jdbcTemplate.queryForObject("select count(*) from Item", Integer.class);
         System.out.println(rowCount);
+    }
+
+    public void testAddStudent() {
+        String sql = "insert into Degree(studentId, degree, graduationTerm," +
+                " graduationYear) values "
+                + "(?, ?, ?, ?); ";
+        PreparedStatement stmt = null;
+
     }
 }
