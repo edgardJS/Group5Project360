b6b750676f360fc9ce9f56eecea85a4cb7be69d0
Adam Waldron <acw86@uw.edu>
2016-11-30 19:57:36 -0800

    Finished initial form.

--- project/src/main/resources/static/js/add-student.js
+++ project/src/main/resources/static/js/add-student.js
@@ -1,7 +1,131 @@
-// wait for the DOM to be loaded
-$(document).ready(function () {
-    // bind 'myForm' and provide a simple callback function
-    $('#myForm').ajaxForm(function () {
-        alert("Thank you for your comment!");
-    });
+$(document).ready(function() {
+    $('#contact_form').bootstrapValidator({
+        // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            id: {
+                validators: {
+                    stringLength: {
+                        min: 6,
+                    },
+                    notEmpty: {
+                        message: 'Please supply your student ID'
+                    }
+                }
+            }
+            firstName: {
+                validators: {
+                    stringLength: {
+                        min: 2,
+                    },
+                    notEmpty: {
+                        message: 'Please supply your first name'
+                    }
+                }
+            },
+            lastName: {
+                validators: {
+                    stringLength: {
+                        min: 2,
+                    },
+                    notEmpty: {
+                        message: 'Please supply your last name'
+                    }
+                }
+            },
+            uwEmail: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please supply your email address'
+                    },
+                    emailAddress: {
+                        message: 'Please supply a valid email address'
+                    }
+                }
+            },
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please supply your email address'
+                    },
+                    emailAddress: {
+                        message: 'Please supply a valid email address'
+                    }
+                }
+            },
+            gpa: {
+                validators: {
+                    stringLength: {
+                        min: 3,
+                    },
+                    double: {
+                        message: 'Please supply a valid GPA'
+                    }
+                }
+            },
+            degreeLevel: {
+                validators: {
+                    stringLength: {
+                        min: 2,
+                    },
+                    notEmpty: {
+                        message: 'Please supply your degree level'
+                    }
+                }
+            },
+            program: {
+                validators: {
+                    stringLength: {
+                        min: 2,
+                    },
+                    notEmpty: {
+                        message: 'Please supply your degree program'
+                    }
+                }
+            },
+            graduationTerm: {
+                validators: {
+                    stringLength: {
+                        min: 4,
+                    },
+                    notEmpty: {
+                        message: 'Please supply your graduation term'
+                    }
+                }
+            },
+            graduationYear: {
+                validators: {
+                    stringLength: {
+                        min: 4,
+                    },
+                    notEmpty: {
+                        message: 'Please supply your graduation year'
+                    }
+                }
+            }
+        }
+    })
+        .on('success.form.bv', function(e) {
+            $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
+            $('#contact_form').data('bootstrapValidator').resetForm();
+
+            // Prevent form submission
+            e.preventDefault();
+
+            // Get the form instance
+            var $form = $(e.target);
+
+            // Get the BootstrapValidator instance
+            var bv = $form.data('bootstrapValidator');
+
+            // Use Ajax to submit form data
+            $.post($form.attr('action'), $form.serialize(), function(result) {
+                console.log(result);
+            }, 'json');
+        });
 });
+
