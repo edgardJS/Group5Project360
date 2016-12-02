f51c2a903a996f45fb640107f81d541346c94b80
Brian <brianjorgenson@live.com>
2016-12-01 20:05:16 -0800

    Test classes, skeletons

---
+++ project/src/main/java/com/project/Tests/DegreeDaoTest.java
@@ -0,0 +1,35 @@
+package com.project.Tests;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Brian on 12/1/2016.
+ */
+public class DegreeDaoTest {
+    @Before
+    public void setUp() throws Exception {
+        
+    }
+    
+    @Test
+    public void addStudentDegree() throws Exception {
+        
+    }
+    
+    @Test
+    public void getStudentDegrees() throws Exception {
+        
+    }
+    
+    @Test
+    public void getDegrees() throws Exception {
+        
+    }
+    
+    @Test
+    public void getStudentDegree() throws Exception {
+        
+    }
+    
+}
