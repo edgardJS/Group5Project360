02d32637792dd457f1dd04ba9cbc9e1ec1b0db73
Brian <brianjorgenson@live.com>
2016-12-01 18:24:09 -0800

    DegreeDao and DB methods added

--- project/src/main/java/com/project/Model/Degree.java
+++ project/src/main/java/com/project/Model/Degree.java
@@ -3,13 +3,14 @@ package com.project.Model;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
-import java.util.List;
 
 /**
  * Created by edgards on 11/9/16.
  */
 @Data
 public class Degree {
+    int degreeId;
+    int studentId;
     String degreeLevel;
     String program;
     String graduationTerm;
