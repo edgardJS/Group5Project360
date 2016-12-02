c3843b9a2795fc19994c213d221ac66f337dc122
Brian <brianjorgenson@live.com>
2016-12-01 18:24:09 -0800

    DegreeDao and DB methods added

--- project/src/main/java/com/project/dao/DegreeDao.java
+++ project/src/main/java/com/project/dao/DegreeDao.java
@@ -4,7 +4,6 @@ import com.project.Model.Degree;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
