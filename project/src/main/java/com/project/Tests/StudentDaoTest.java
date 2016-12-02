8458bfa050e075e3ffbddbf15d60b29ef2e08dc0
Brian <brianjorgenson@live.com>
2016-12-01 20:05:16 -0800

    Test classes, skeletons

---
+++ project/src/main/java/com/project/Tests/StudentDaoTest.java
@@ -0,0 +1,35 @@
+package com.project.Tests;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Brian on 12/1/2016.
+ */
+public class StudentDaoTest {
+    @Before
+    public void setUp() throws Exception {
+        
+    }
+    
+    @Test
+    public void testAddStudent() throws Exception {
+        
+    }
+    
+    @Test
+    public void testUpdateStudent() throws Exception {
+        
+    }
+    
+    @Test
+    public void getStudent() throws Exception {
+        
+    }
+    
+    @Test
+    public void getStudents() throws Exception {
+        
+    }
+    
+}
